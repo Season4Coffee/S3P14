42b0270cee2d2aa026279a27c2167e9bbe3d627e
Season4Coffee <as8108865@gmail.com>
2024-12-06 14:03:04 +0300

    Create tel.h

---
+++ Telephone/tel.h
@@ -0,0 +1,17 @@
+#ifndef TEL_H
+#define TEL_H
+
+#include <bits/stdc++.h>
+using namespace std;
+class tel{
+public:
+tel();
+tel(int x,int y)
+int col(int x);
+int zaed(int y);
+private:
+int moned;
+int telcol;
+int vk;
+}
+#endif // TEL_H
