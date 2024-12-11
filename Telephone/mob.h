9a9b9ffbe5ce5ccee466b116d36346a9e79b364d
Season4Coffee <as8108865@gmail.com>
2024-12-11 21:28:35 +0300

    Create mob.h

---
+++ Telephone/mob.h
@@ -0,0 +1,13 @@
+#ifndef MOB_H
+#define MOB_H
+#include "tel.h"
+class mob: public tel{
+public:
+mob(string x, int y, string z);
+void settype(string z);
+void show();
+protected:
+private:
+string typecn;
+};
+#endif // MOB_H
