300b93155b4ab1e9ee8b09aca47261a5e6ceb95e
Season4Coffee <as8108865@gmail.com>
2024-12-12 21:51:24 +0300

    Update tel.h

--- Telephone/tel.h
+++ Telephone/tel.h
@@ -5,10 +5,12 @@ using namespace std;
 class tel{
 public:
 tel();
-tel(string x,int y)
+tel(string x,int y);
+int getcena();
 void setmodel(string x);
 void setcena(int y);
 virtual void show();
+virtual void edit();
 protected:
 string model;
 int cena;
