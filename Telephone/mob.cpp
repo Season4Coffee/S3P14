8e3cb238fb7a605e59c94ec7a91ed0c50b370b73
Season4Coffee <as8108865@gmail.com>
2024-12-12 22:06:16 +0300

    Update mob.cpp

--- Telephone/mob.cpp
+++ Telephone/mob.cpp
@@ -2,6 +2,7 @@
 
 void mob:settytpe(string z){
   typecn = z;
+  cout << "Тип связи -" << " " << typecn << endl;
 }
 mob:mob(string x, int y, string z){
   model = x;
@@ -11,3 +12,15 @@ mob:mob(string x, int y, string z){
 void tel:show(){
   cout << "Стоимость =" << " " << cena << " " << "Модель телефона -" << " " << model << " " << "Тип связи -" << " " << typecn << endl;
 }
+void tel::edit(){
+  cout << "Стоимость?\n";
+  cin >> y;
+  setcena(int y);
+  cout << "Модель?\n";
+  cin << x;
+  setmodel(sring y);
+  cout << "Тип связи?\n";
+  cin >> z;
+  setves(string z);
+}
+
