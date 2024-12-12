85934972c8a62cf31aff187affd6b83e38128a0e
Season4Coffee <as8108865@gmail.com>
2024-12-12 22:04:12 +0300

    Update stat.cpp

--- Telephone/stat.cpp
+++ Telephone/stat.cpp
@@ -2,6 +2,7 @@
 
 void stat:setves(int w){
   ves = w;
+  cout << "Вес кирпича =" << " " << ves << endl;
 }
 stat:stat(string x, int y, int w){
   model = x;
@@ -10,4 +11,15 @@ stat:stat(string x, int y, int w){
 }
 void tel:show(){
   cout << "Стоимость =" << " " << cena << " " << "Модель телефона -" << " " << model << " " << "Вес кирпича =" << " " << ves << endl;
+}
+void tel::edit(){
+  cout << "Стоимость?\n";
+  cin >> y;
+  setcena(int y);
+  cout << "Модель?\n";
+  cin << x;
+  setmodel(sring y);
+  cout << "Вес?\n";
+  cin >> w;
+  setves(int w);
 }
