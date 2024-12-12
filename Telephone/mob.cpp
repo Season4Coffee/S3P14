18aeff4c1c9f2198507528a979797c90fecfe299
Season4Coffee <as8108865@gmail.com>
2024-12-12 21:14:11 +0300

    Update mob.cpp

--- Telephone/mob.cpp
+++ Telephone/mob.cpp
@@ -1,17 +1,12 @@
 #include "..\include\mob.h"
- void mob:settytpe(string z){
+
+void mob:settytpe(string z){
   typecn = z;
 }
 mob:mob(string x, int y, string z){
   model = x;
   cena = y;
   typecn = z;
-}
-void tel:setcena(int y){
-  cena = y;
-}
-void tel:setmodel(string x){
-  model = x;
 }
 void tel:show(){
   cout << "Стоимость =" << " " << cena << " " << "Модель телефона -" << " " << model << " " << "Тип связи -" << " " << typecn << endl;
